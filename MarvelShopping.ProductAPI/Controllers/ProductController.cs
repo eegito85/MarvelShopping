e9be5b389ba2215c2256d345e9648af49e5e037a
EMMANUEL EGITO OLIVEIRA CANDEIRA MARINHO <e_egito@hotmail.com>
2022-01-14 08:45:18 -0300

    Criação das controllers e views na parte Web

--- MarvelShopping.ProductAPI/Controllers/ProductController.cs
+++ MarvelShopping.ProductAPI/Controllers/ProductController.cs
@@ -33,7 +33,7 @@ namespace MarvelShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO productVO)
         {
             if (productVO == null) return BadRequest();
             var product = await _repository.Create(productVO);
@@ -41,7 +41,7 @@ namespace MarvelShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO productVO)
         {
             if (productVO == null) return BadRequest();
             var product = await _repository.Update(productVO);
---
+++ MarvelShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using MarvelShopping.Web.Models;
+
+namespace MarvelShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> FindProductByName(ProductModel model);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
