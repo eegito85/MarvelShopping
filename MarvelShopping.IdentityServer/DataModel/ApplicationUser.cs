b258f5074069dfe2532ac7ecad68155b0008ede0
EMMANUEL EGITO OLIVEIRA CANDEIRA MARINHO <e_egito@hotmail.com>
2022-01-19 00:57:07 -0300

    criação do projeto IdentityServer

---
+++ MarvelShopping.IdentityServer/DataModel/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MarvelShopping.IdentityServer.DataModel
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+    }
+}
---
+++ MarvelShopping.IdentityServer/DataModel/Context/MySQLContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarvelShopping.IdentityServer.DataModel.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Product> Products { get; set; }
+    }
+}
